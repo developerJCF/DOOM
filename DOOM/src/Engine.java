c009df579c7dc63878b4d8999db6284e7eb8c783
avate <developerJCF@gmail.com>
2018-07-03 07:21:29 -0500

    Engine Class Created

---
+++ DOOM/src/Engine.java
@@ -0,0 +1,14 @@
+
+public class Engine {
+
+	public static void main(String[] args) {
+		
+		//Camera class = control camera
+		//Map Loading class to control map data and such
+		//Collision Detection
+		//Texture loading class
+		
+
+	}
+
+}
